87ca3003675dd15e0ad1d7067abbe0f0e66f02bd
K. Hristev <K. Hristev@HRAGADAGAL>
2021-04-04 16:07:31 +0300

    Changes

--- Flight Manager/Data/ApplicationDbContext.cs
+++ Flight Manager/Data/ApplicationDbContext.cs
@@ -12,7 +12,6 @@ namespace Flight_Manager.Data
     {
        // internal IList<FlightViewModel> Flights;
        public DbSet<Flight> Flights { get; set; }
-
         public DbSet<User> Users { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
