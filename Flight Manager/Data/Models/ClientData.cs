2439100fff731a4442b42508476c2bd4d27cfecf
K. Hristev <K. Hristev@HRAGADAGAL>
2021-04-11 08:51:01 +0300

    Coments

--- Flight Manager/Data/Models/ClientData.cs
+++ Flight Manager/Data/Models/ClientData.cs
@@ -12,13 +12,19 @@ namespace Flight_Manager.Data.Models
     {
 
         [Required]
+        //Име на клиента
         public string FirstName { get; set; }
+        // Презиме на клиента
         public string MiddleName { get; set; }
+        //Фамилия на клиента
         public string LastName { get; set; }
         [Key]
+        //ID поле(уникално)
         public string PersonalId { get; set; }
+        //Поле за телефонен номер
         public string TelephoneNumber { get; set; }
         public string Nationality { get; set; }
+        //Поле от тип TicketType за вид на билета 
         public TicketType ticketType { get; set; }
     }
 }
--- Flight Manager/Data/Models/User.cs
+++ Flight Manager/Data/Models/User.cs
@@ -8,14 +8,22 @@ using System.Threading.Tasks;
 
 namespace Flight_Manager.Data.Models
 {
+    //Клас - таблица User наследяваща IdentityUser
     public class User : IdentityUser
     {
+        /// <summary>
+        /// Поле за Име на User-а
+        /// </summary>
         [Required(ErrorMessage ="First name is required")]        
         public string FirstName { get; set; }
         [Required(ErrorMessage = "Last name is required")]
+        //Поле за фамилия 
         public string LastName { get; set; }
         [StringLength(10)]
+
+        // ID поле(уникално)
         public string PersonalId { get; set; }
+        //Поле за Адрес
         public string Addres { get; set; }
     }
 }
